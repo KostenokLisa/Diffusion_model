37233f9540d613a865210c615ab109946481dccb
KostenokLisa <65138661+KostenokLisa@users.noreply.github.com>
2021-09-05 15:11:02 +0300

    Add N-dimensional version

--- data_preparing.py
+++ data_preparing.py
@@ -1,21 +1,31 @@
-from numpy.random import normal
-from math import sqrt
+import numpy as np
+from numpy.random import multivariate_normal
+from numpy.random import binomial
 import matplotlib.pyplot as plt
 
 
-# dataset consists of N floats sampled from gmm distribution
-def create_dataset(N, mean1, mean2, var1, var2, p):
+# dataset consists of M vectors sampled from N-dimensional gmm distribution
+def create_dataset(M, mean1, mean2, var1, var2, p):
+    idx1 = binomial(1, p, M)
+    idx2 = np.ones(M) - idx1
 
-    data1 = normal(mean1, sqrt(var1), size=N)
-    data2 = normal(mean2, sqrt(var2), size=N)
+    data1 = multivariate_normal(mean1, np.diag(np.sqrt(var1)), M)
+    data2 = multivariate_normal(mean2, np.diag(np.sqrt(var2)), M)
 
-    gmm_data = p * data1 + (1 - p) * data2
+    gmm_data = np.diag(idx1) @ data1 + np.diag(idx2) @ data2
     return gmm_data
 
 
-def draw_distribution(gmm_data, N):
-    step = 100
+# 1-D or 2-D distribution visualization
+def draw_distribution(gmm_data):
+    dim = gmm_data.shape
+    N = len(dim) - 1
+    if N == 1:
+        step = 100
+        fig, ax = plt.subplots(figsize=(30, 20))
+        ax.hist(gmm_data, step)
+    else:
+        plt.scatter(gmm_data.T[0], gmm_data.T[1])
+        plt.title("2dim distribution")
 
-    fig, ax = plt.subplots(figsize=(30, 20))
-    ax.hist(gmm_data, int(N / step))
     plt.show()

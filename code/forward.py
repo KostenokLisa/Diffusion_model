dddf336f45cf2935327bbda77329c691a2fcc27b
KostenokLisa <65138661+KostenokLisa@users.noreply.github.com>
2021-09-05 16:18:15 +0300

    Add N-dimensional version

--- code/forward.py
+++ code/forward.py
@@ -1,16 +1,20 @@
-from numpy.random import normal
+import numpy as np
+from numpy.random import multivariate_normal
 from math import sqrt
 
 
-# for x sample and save all latent variables, T - number of steps in forward process, betas - hyperparameters
+# for N-dimensional input x sample and save all latent variables,
+# T - number of steps in forward process, betas - hyperparameters
 def sample_latent_var(betas, x):
     samples = [x]
     T = len(betas)
+    N = len(x)
+    x = np.array(x)
 
     for i in range(T):
         mean = sqrt(1 - betas[i]) * x
-        scale = sqrt(betas[i])
-        x = normal(mean, scale)
+        scale = sqrt(betas[i]) * np.eye(N)
+        x = multivariate_normal(mean, scale, size=1)[0]
         samples.append(x)
 
-    return samples
+    return np.array(samples)

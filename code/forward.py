174f6fa1ec33f141240ce079b657d69a085be7fd
KostenokLisa <65138661+KostenokLisa@users.noreply.github.com>
2022-02-21 21:15:11 +0300

    added naive version of reverse process which predicts mean and variance

--- code/forward.py
+++ code/forward.py
@@ -2,81 +2,72 @@ import torch
 
 DEVICE = torch.device("cuda")
 
+# creating dictionary of constants used while estimating prior and posterior distributions
+def estimate_a_b_parameters(betas):
+    param_dict = {}
 
-def naive_forward_process(data_loader, n_steps, betas):
-    # gradually adding gaussian noise to tensors in batch with fixed betas parameter
-    res_seq = []
-    for inputs, labels in data_loader:
-        inputs = inputs.to(DEVICE)
-        #labels = labels.to(DEVICE)  labels can be used for conditional generation
-        batch_start = inputs
-        batch_seq = [batch_start]
-        for n in range(n_steps):
-            batch_seq.append((torch.sqrt(1 - betas[n]) * batch_seq[-1]) + (betas[n] * torch.rand_like(batch_start)))
-        res_seq.append(batch_seq)
-    return res_seq
+    alphas = 1 - betas
+    alphas_prod = torch.cumprod(alphas, 0)
+    alphas_prod_p = torch.cat([torch.tensor([1]).float(), alphas_prod[:-1]], 0)
+    alphas_bar_sqrt = torch.sqrt(alphas_prod)
+    one_minus_alphas_bar_log = torch.log(1 - alphas_prod)
+    one_minus_alphas_bar_sqrt = torch.sqrt(1 - alphas_prod)
+    post_mean_coef_1 = betas * torch.sqrt(alphas_prod_p / (1 - alphas_prod))
+    post_mean_coef_2 = (1 - alphas_prod_p) * torch.sqrt(alphas) / (1 - alphas_prod)
+    post_variance = betas * (1 - alphas_prod_p) / (1 - alphas_prod)
+    post_log_variance = torch.log(torch.cat((post_variance[1].view(1, 1), post_variance[1:].view(-1, 1)), 0)).view(-1)
 
+    param_dict["betas"] = betas
+    param_dict["alphas"] = alphas
+    param_dict["alphas_prod"] = alphas_prod
+    param_dict["alphas_bar_sqrt"] = alphas_bar_sqrt
+    param_dict["one_minus_alphas_bar_log"] = one_minus_alphas_bar_log
+    param_dict["one_minus_alphas_bar_sqrt"] = one_minus_alphas_bar_sqrt
+    param_dict["post_mean_coef_1"] = post_mean_coef_1
+    param_dict["post_mean_coef_2"] = post_mean_coef_2
+    param_dict["post_variance"] = post_variance
+    param_dict["post_log_variance"] = post_log_variance
 
-def make_beta_schedule(schedule, n_timesteps, start, end):
-    # variation of betas parameter depending on schedule mode
+    return param_dict
+
+def make_beta_schedule(schedule='linear', n_timesteps=1000, start=1e-5, end=1e-2):
     if schedule == 'linear':
         betas = torch.linspace(start, end, n_timesteps)
     elif schedule == "quad":
         betas = torch.linspace(start ** 0.5, end ** 0.5, n_timesteps) ** 2
     elif schedule == "sigmoid":
-        limit = 6  # set limits for sigmoid argument
-        betas = torch.linspace(-limit, limit, n_timesteps)
+        betas = torch.linspace(-6, 6, n_timesteps)
         betas = torch.sigmoid(betas) * (end - start) + start
     return betas
 
 
-def get_alphas(betas):
-    # estimation of alpha parameter depending on betas variation
-    alphas = 1 - betas
-    alphas_prod = torch.cumprod(alphas, 0)
-    return torch.sqrt(alphas_prod), torch.sqrt(1 - alphas_prod)
+def extract(input, t, x):
+    shape = x.shape
+    out = torch.gather(input, 0, t.to(input.device))
+    reshape = [t.shape[0]] + [1] * (len(shape) - 1)
+    return out.reshape(*reshape)
 
 
-def extract(input, t, x_batch):
-    # getting alpha parameters in required number of dimensions
-    shape = x_batch.shape
-    N = len(shape)
-    output = torch.gather(input, 0, t.to(DEVICE))
-    new_shape = [t.shape[0]] + [1] * (N - 1)
-    return output.reshape(*new_shape)
-
-
-def q_sample(x_batch, t, betas, noise):
-    # more efficient forward process for sampling at a fixed timestep t
+def q_sample(x_0, t, param_dict, noise=None):
     if noise is None:
-        noise = torch.randn_like(x_batch).to(DEVICE)
-    alphas_bar_sqrt, one_minus_alphas_bar_sqrt = get_alphas(betas)
-    alphas_t = extract(alphas_bar_sqrt, t, x_batch)
-    alphas_1_m_t = extract(one_minus_alphas_bar_sqrt, t, x_batch)
+        noise = torch.randn_like(x_0)
+    alphas_t = extract(param_dict["alphas_bar_sqrt"], t, x_0)
+    alphas_1_m_t = extract(param_dict["one_minus_alphas_bar_sqrt"], t, x_0)
+    return alphas_t * x_0 + alphas_1_m_t * noise
 
-    alphas_1_m_t = alphas_1_m_t.to(DEVICE)
-    alphas_t = alphas_t.to(DEVICE)
-    return alphas_t * x_batch + alphas_1_m_t * noise
-
-
-def calc_posterior(betas, alphas, alphas_prod_p, alphas_prod):
-    # estimating posterior distribution parameters based on beta and alpha parameters
-    post_mean_coef_1 = (betas * torch.sqrt(alphas_prod_p) / (1 - alphas_prod))
-    post_mean_coef_2 = ((1 - alphas_prod_p) * torch.sqrt(alphas) / (1 - alphas_prod))
-    post_variance = betas * (1 - alphas_prod_p) / (1 - alphas_prod)
-    post_log_variance = torch.log(torch.cat((post_variance[1].view(1, 1), post_variance[1:].view(-1, 1)), 0))
-    return post_mean_coef_1, post_mean_coef_2, post_log_variance.view(-1)
-
-
-def q_posterior_mean_variance(betas, alphas, alphas_prod_p, alphas_prod, t, x_0, x_t):
-    # estimating posterior distribution parameters at a fixed timestep t
-    post_params = calc_posterior(betas, alphas, alphas_prod_p, alphas_prod)
-    post_mean_coef_1 = post_params[0]
-    post_mean_coef_2 = post_params[1]
-    post_log_variance = post_params[2]
-
-    coef_1 = extract(post_mean_coef_1, t, x_0)
-    coef_2 = extract(post_mean_coef_2, t, x_0)
+def q_posterior_mean_variance(x_0, x_t, t, param_dict):
+    coef_1 = extract(param_dict["posterior_mean_coef_1"], t, x_0)
+    coef_2 = extract(param_dict["posterior_mean_coef_2"], t, x_0)
     mean = coef_1 * x_0 + coef_2 * x_t
-    var = extract(post_log_variance, t, x_0)
+    var = extract(param_dict["posterior_log_variance_clipped"], t, x_0)
     return mean, var
+
+
+
+
+
+
+
+
+
+
